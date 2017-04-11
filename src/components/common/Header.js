6afbf8ff77083781ce8c8134ca29b5e746b7c37e
Murph Grainger <murph.grainger@gmail.com>
2017-04-11 16:53:42 -0600

    added redux

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -6,9 +6,11 @@ const Header = () => {
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
       {" | "}
+      <Link to ="/courses" activeClassName="active">Courses</Link>
+        {" | "}
       <Link to ="/about" activeClassName="active">About</Link>
     </nav>
-  )
+  );
 }
 
 
